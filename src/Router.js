45367013e92c574e895d5b4342c610887db9da9d
yining0908 <taening0908@gmail.com>
2017-04-21 00:44:44 +0800

    Add files via upload

---
+++ src/Router.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+import { Icon } from 'react-native-elements';
+
+import Channel from './components/Channel';
+import Details from './components/Details';
+import Account from './components/Account';
+import Setting from './components/Setting';
+import Collection from './components/Collection';
+import lan from './components/lan';
+
+export const ChannelStack = StackNavigator({
+Channel: {
+    screen: Channel,
+    navigationOptions: {
+      header: () => ({
+        title: 'CHANNEL',
+      })
+    },
+  },
+Details: {
+screen: Details,
+navigationOptions: {
+header: () => ({
+ title: 'BBC LEARNING',
+})
+ },
+ },
+});
+
+export const AccountStack = StackNavigator({
+  Account: {
+    
+    screen: Account,
+    navigationOptions: {
+      header: () => ({
+        title: 'ACCOUNT',
+      })
+    },
+  },
+},
+{
+  // headerMode: 'none',
+}
+);
+
+export const SettingStack = StackNavigator({
+  Setting: {
+    screen: Setting,
+    navigationOptions: {
+      header: () => ({
+        title: 'SETTING',
+      })
+    },
+  },
+ lan: {
+screen:lan,
+navigationOptions: {
+header: () => ({
+ title: 'LANGUAGE',
+})
+ },
+ },
+});
+export const CollectionStack = StackNavigator({
+  Collection: {
+    screen: Collection,
+    navigationOptions: {
+      header: () => ({
+        title: 'COLLECTION',
+      })
+    },
+  },
+});
+
+export const TabRouter = TabNavigator(
+  {
+   ChannelStack: {
+      screen: ChannelStack,
+      navigationOptions: {
+        tabBar: {
+          label: 'Channel',
+          icon: ({ tintColor }) => <Icon name="tv" size={32} color={tintColor} />
+        },
+      },
+    },
+     CollectionStack: {
+      screen: CollectionStack,
+      navigationOptions: {
+        tabBar: {
+          label: 'Collection',
+          icon: ({ tintColor }) => <Icon name="bookmark" size={32} color={tintColor} />
+          
+        },
+      },
+    },
+    AccountStack: {
+      screen: AccountStack,
+      navigationOptions: {
+        tabBar: {
+          label: 'Account',
+          icon: ({ tintColor }) => <Icon name="account-circle" size={32} color={tintColor} />
+        },
+      },
+    },
+    SettingStack: {
+      screen: SettingStack,
+      navigationOptions: {
+        tabBar: {
+          label: 'Setting',
+          icon: ({ tintColor }) => <Icon name="settings" size={32} color={tintColor} />
+        },
+      },
+    },
+  },
+  {
+    animationEnabled: 'true',
+  }
+ 
+);
+
+
